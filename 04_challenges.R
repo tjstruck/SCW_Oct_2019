145c77a36fb38ce33dc7523f6c6859b2ed27e1d8
kelseygonzalez <kelseygonzalez@email.arizona.edu>
2019-10-16 22:48:07 -0700

    Add challenges file

---
+++ 04_challenges.R
@@ -0,0 +1,6 @@
+# We need to implement the challenge here. Let's go through 
+# http://swcarpentry.github.io/r-novice-gapminder/05-data-structures-part2/index.html
+# http://swcarpentry.github.io/r-novice-gapminder/07-control-flow/index.html
+# http://swcarpentry.github.io/r-novice-gapminder/08-plot-ggplot2/index.html
+# to find some possibly good challenges. Especially useful if we can find challenges
+# that can make life easier using dplyr which will be taught afterwards. 
