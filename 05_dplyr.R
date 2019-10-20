56fa13263ba174986d8728c58b956ec9c25a5e48
kelseygonzalez <kelseygonzalez@email.arizona.edu>
2019-10-20 16:33:41 -0700

    Update lost work

--- 05_dplyr.R
+++ 05_dplyr.R
@@ -1,5 +1,4 @@
-library(tidyverse)
-gapminder <- read.csv("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder_data.csv", stringsAsFactors = FALSE)
+gapminder <- read.csv("gapminder_clean.csv", stringsAsFactors = FALSE)
 
 #How to install a new package
 install.packages("dplyr")
@@ -25,73 +24,94 @@ filter(gapminder, year > 2000)
 two.countries <- c('Kenya', 'Gibon')
 filter(gapminder, country %in% two.countries)
 
-
-
 #%in% will enable you to search all lines in the column country for all character strings in the two.countries file 
 #and will return a TRUE if it finds an one of them.
+
+
 
 # dplyr works by piping commands, like you learned to do in the command line.  
 # Instead of the pipe `|`, we use `%>%`.
 gapminder %>% select(lifeExp) %>% min()
-
 #How we would do it without dplyr
 min(gapminder[,'lifeExp'])
-#An important difference between `dplyr` and base R is when use character strings 
-#we don't need to enclose them in quotation marks as we did above (i.e. gapminder[,'lifeExp'])
 
 #But we can do more complex things with dplyr
 gapminder %>% select(-pop, -gdpPercap) %>% filter(continent == "Asia",year == 2007) 
 
 
-# 
-mutate()
-
-#If we want to make a new column, use `mutate`.  Don't forget we have to assign it if we want to keep the changes
+# mutate()
+# If we want to make a new column, use `mutate`.  Don't forget we have to assign it if we want to keep the changes
 gapminder <- gapminder %>% mutate(NewColumn = lifeExp * 12)
 gapminder
 
 
-# Group_by() divides the dataframe into multiple data frames for each group value.
-group_by()
+#Challenge 1
+# Write a single command (which can span multiple lines and includes pipes) that will produce a 
+# dataframe that has the African values for lifeExp, country and year, but not for other Continents. 
+# How many rows does your dataframe have and why?
+year_country_lifeExp_Africa <- gapminder %>%
+  filter(continent == "Africa") %>%
+  select(year, country, lifeExp)  
+  
 
-# summarize. Group_by() is much more exciting in conjunction with summarize(). This will allow 
+# Group_by() divides the dataframe into multiple data frames for each group value.
+# Group_by() is much more exciting in conjunction with summarize(). This will allow 
 # us to create new variable(s) by using functions that repeat for each of the continent-specific 
 # data frames. That is to say, using the group_by() function, we split our original dataframe into 
 # multiple pieces, then we can run functions (e.g. mean() or sd()) within summarize().
+gapminder %>% 
+  group_by(continent)
+# gives us a data frame grouped by continent
+
+# but that's not particularly useful. How about we get the mean life expectancy by continent? 
+ gapminder %>% 
+   group_by(continent) %>% 
+   summarize(MeanLife = mean(lifeExp))
+
+# How about by continent and year? 
+gapminder %>% 
+  group_by(continent, year) %>% 
+  summarize(MeanLife = mean(lifeExp))
+
+# Let's add on the standard deviation as well and save the dataframe  
+continent_year <- gapminder %>% 
+  group_by(continent, year) %>%
+  summarize(MeanLife = mean(lifeExp),
+            SdLife = sd(lifeExp)/sqrt(n()))
+
+# let's plot this!
+require(ggplot2)
+ggplot(data = continent_year, 
+         mapping = aes(x = year, y = MeanLife, color=continent)) + 
+   geom_line() + 
+   geom_errorbar(mapping = aes(ymin = MeanLife - SdLife, 
+                                  ymax = MeanLife + SdLife,
+                                  width = 0.3))
 
 
-summarize()
 
-count()
-n()
+# Challenge 2 
+# Create a new dataframe that contains the minimum (MinExp) and maximum (MaxExp) life 
+# expectancies for each country.
 
+MinMaxExp <- gapminder %>% 
+  group_by(country) %>% 
+  summarize(MinExp = min(lifeExp),
+            MaxExp = max(lifeExp))
 
-gapminder %>%
-  group_by(continent) %>%
-  summarize(
-    mean_le = mean(lifeExp),
-    min_le = min(lifeExp),
-    max_le = max(lifeExp),
-    se_le = sd(lifeExp)/sqrt(n()))
+# Challenge 3
+# Calculate the average life expectancy per country. Which has the longest average life 
+# expectancy and which has the shortest average life expectancy?
 
-
-
-
-
-
-
-
-
-
-#Now what about subsetting rows?  For this we use te `filter` command:
-gapminder %>% filter(lifeExp > 71.5)
+lifeExp_bycountry <- gapminder %>%
+  group_by(country) %>%
+  summarize(mean_lifeExp = mean(lifeExp))
+lifeExp_bycountry %>%
+  filter(mean_lifeExp == min(mean_lifeExp) | mean_lifeExp == max(mean_lifeExp))
+  
 
 
 
-#EXERCISE
-#Reimport the `gapminder` dataframe:
-#Create a new dataframe that contains only country names, years, and life expectancies of the `gapminder` dataset. 
-#Use this new dataframe to calculate minimum & maximum expectancies.
 
-# dplyr
-gapminder %>% select(continent) %>% distinct()
+
+
